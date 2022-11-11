45b6a569661b02bf4fc53b3093517d44e1761add
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-11-11 16:35:09 +0300

    fix: fix path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    main: './src/index.js',
+    main: './src/pages/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
