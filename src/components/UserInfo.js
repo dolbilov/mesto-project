eb2a9d7eed4c08279a6879a7a41e2d47bea73f4f
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-11-13 10:26:14 +0300

    chore!: totally change class structure

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,23 @@
-import { profileName, profileDescription } from "../utils/constants";
-
-class UserInfo {
-  constructor(userId) {
-    this._userId = userId;
+export default class UserInfo {
+  constructor({ profileNameSelector, profileAboutSelector, profileAvatarSelector }) {
+    this._profileName = document.querySelector(profileNameSelector);
+    this._profileAbout = document.querySelector(profileAboutSelector);
+    this._profileAvatar = document.querySelector(profileAvatarSelector);
   }
 
-  hasUserLike = (likes) => likes.some(like => like === this._userId);
+  getUserInfo(getUserInfoCallback) {
+    return getUserInfoCallback();
+  }
 
-  isCreatedByUser = (ownerId) => ownerId === this._userId;
-
-  updateUserInfo = ({ name, about }) => {
-    profileName.textContent = name;
-    profileDescription.textContent = about;
+  setUserInfo({ name, about, avatar }, api) {
+    api.setUserInfo(name, about)
+      .then((data) => {
+        this._profileName.textContent = data.name;
+        this._profileAbout.textContent = data.about;
+      });
+    api.setAvatar(avatar)
+      .then((data) => {
+        this._profileAvatar.src = data.avatar;
+    });
   }
 }
